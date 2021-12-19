b9967bcce69bca63fa49735c29262b91f7cda040
Chris Coffin <948066+magnolialogic@users.noreply.github.com>
2021-12-18 20:24:50 -0500

    Update StarscreamClient.swift
    Clean up URL construction and remove unnecessary/ignored timeout

--- iOS/Model/StarscreamClient.swift
+++ iOS/Model/StarscreamClient.swift
@@ -28,11 +28,11 @@ class StarscreamClient: WebSocketDelegate {
 	var socket: WebSocket?
 	
 	func connect() {
-		let requestURL = URL(string: VaporClient.shared.url.replacingOccurrences(of: "http", with: "ws") + "/api/client/ws")!
+		let requestURLString = VaporClient.shared.url.components(separatedBy: "://")[0].replacingOccurrences(of: "http", with: "ws") + "://" + VaporClient.shared.url.components(separatedBy: "://")[1] + "/api/client/upgrade" // Change scheme from http(s) -> ws(s) and add route
+		let requestURL = URL(string: requestURLString)!
 		MLLogger.console(String(describing: requestURL))
 		var wsRequest = URLRequest(url: requestURL)
 		wsRequest.setBasicAuth(username: "app", password: VaporClient.shared.secretKey)
-		wsRequest.timeoutInterval = 30
 		self.socket = WebSocket(request: wsRequest)
 		self.socket?.delegate = self
 		socket?.connect()
@@ -60,7 +60,6 @@ class StarscreamClient: WebSocketDelegate {
 		case .reconnectSuggested(let suggestion):
 			MLLogger.console("reconnect suggested \(String(describing: suggestion))")
 		case .error(let error):
-			self.connected = false
 			var message: String
 			if let e = error as? WSError {
 				message = e.message
@@ -70,6 +69,7 @@ class StarscreamClient: WebSocketDelegate {
 				message = "unhandled error"
 			}
 			MLLogger.error(message)
+			self.connected = false
 		default: // .ping(_), .pong(_), .text(_)
 			break
 		}
